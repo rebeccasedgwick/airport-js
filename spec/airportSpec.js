003ed12fb5521137e253b6bf819e0f86cc2590c8
Rebecca Sedgwick <36798938+rebeccasedgwick@users.noreply.github.com>
2018-09-07 12:36:56 +0100

    Adds weather, and conditional take off

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -15,10 +15,30 @@ describe('Feature tests', function() {
   });
 
   it('planes can take off from the airport', function() {
+    spyOn(airport, 'isBadWeather').and.returnValue(false)
     airport.land(plane);
     airport.takeOff(plane);
     expect(airport.hangar()).not.toContain(plane);
   });
 
+  describe('Weather conditions', function() {
 
+    it('checks for Stormy (true) or Sunny (false) weather', function() {
+      expect([true, false]).toContain(airport.isBadWeather());
+    });
+
+    it('takeOff is prevented if weather is stormy', function() {
+      spyOn(airport, 'isBadWeather').and.returnValue(true);
+      airport.land(plane);
+      expect(function() {airport.takeOff()}).toThrowError('Too stormy to take off!')
+      expect(airport.hangar()).toContain(plane);
+    });
+
+    it('takeOff is allowed if weather is sunny', function() {
+      airport.land(plane);
+      spyOn(airport, 'isBadWeather').and.returnValue(false)
+      airport.takeOff(plane);
+      expect(airport.hangar()).not.toContain(plane);
+    });
+  });
 });
--- src/airport.js
+++ src/airport.js
@@ -1,5 +1,5 @@
 function Airport() {
-  this.landedPlanes = []
+  this.landedPlanes = [];
 };
 
 Airport.prototype.hangar = function() {
@@ -11,5 +11,13 @@ Airport.prototype.land = function(plane) {
 };
 
 Airport.prototype.takeOff = function(plane) {
+  if (this.isBadWeather()) {
+    throw new Error ('Too stormy to take off!');
+  }
   this.landedPlanes.splice((this.landedPlanes.indexOf(plane)), 1);
-}
+};
+
+Airport.prototype.isBadWeather = function() {
+  var weather = [true, false];
+  return weather[Math.floor(Math.random() * weather.length)];
+};
